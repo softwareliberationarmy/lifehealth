00183a802a9182119cbeac8c3c978b73dc074ed8
Kerry Patrick <kpatrick@mmmicrosystems.com>
2021-08-11 13:07:06 -0500

    adding simple 404 handling

--- src/app.js
+++ src/app.js
@@ -3,10 +3,16 @@ const express = require('express');
 
 const app = express();
 
+//serve static files from public folder
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get('/', (_r, res, _n) => {
     res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+//last resort handler
+app.use((req, res, next) => {
+    res.sendFile(path.join(__dirname, "views", "notfound.html"));
 });
 
 app.listen(3000);
