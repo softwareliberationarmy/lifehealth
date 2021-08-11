507cb2444ebe8a42b250dd414c666dc678aa4e09
Kerry Patrick <kpatrick@mmmicrosystems.com>
2021-08-11 13:53:31 -0500

    adding unit testing to showHtmlFile. installing jest-extended as a dev dependency and adding to jest configuration

---
+++ src/helpers/route-handlers.test.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const showHtmlFile = require('./route-handlers');
+
+describe('route handling helper methods', () => {
+    describe('showHtmlFile', () => {
+        it('should return a func that sends a file to the response object', () => {
+            const result = showHtmlFile('breadbox.html');
+            const spyResponse = {                 
+                sendFile:  function(fullPath){
+                    this.lastPathSent = fullPath;
+                }
+            };
+
+            result(null, spyResponse);
+
+            expect(spyResponse.lastPathSent).toEndWith(path.join('src', 'views', 'breadbox.html'));
+        });
+    });
+})
