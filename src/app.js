524a2601f2debae3153781fdd1c67492d6034692
Kerry Patrick <kpatrick@mmmicrosystems.com>
2021-08-11 13:28:21 -0500

    adding under construction page, and creating helper function to reduce duplicated code with route handlers

--- src/app.js
+++ src/app.js
@@ -3,16 +3,23 @@ const express = require('express');
 
 const app = express();
 
+const showHtmlFile = (filename) => {
+    return (_r, res, _n) => {
+        res.sendFile(path.join(__dirname, 'views', filename));
+    }
+}
+
 //serve static files from public folder
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/', (_r, res, _n) => {
-    res.sendFile(path.join(__dirname, 'views', 'index.html'));
-});
+app.get('/', showHtmlFile('index.html'));
+
+//construction handler
+for(const route of ['/running', '/weight', '/workouts']){
+    app.get(route, showHtmlFile('construction.html'));
+}
 
 //last resort handler
-app.use((req, res, next) => {
-    res.sendFile(path.join(__dirname, "views", "notfound.html"));
-});
+app.use(showHtmlFile('notfound.html'));
 
 app.listen(3000);
