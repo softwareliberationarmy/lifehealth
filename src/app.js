5f18572a2165f97b3aaefc41e7c7b32959b2b563
Kerry Patrick <kpatrick@mmmicrosystems.com>
2021-08-11 13:33:16 -0500

    extracting helper method to separate js file

--- src/app.js
+++ src/app.js
@@ -1,13 +1,8 @@
 const path = require('path');
 const express = require('express');
+const showHtmlFile = require('./helpers/route-handlers');
 
 const app = express();
-
-const showHtmlFile = (filename) => {
-    return (_r, res, _n) => {
-        res.sendFile(path.join(__dirname, 'views', filename));
-    }
-}
 
 //serve static files from public folder
 app.use(express.static(path.join(__dirname, "public")));
