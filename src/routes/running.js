ae13d1793543d65a6d46e656564fc20f93f67966
Kerry Patrick <kpatrick@mmmicrosystems.com>
2021-08-15 21:21:03 -0500

    fixing CSS, adding separate run page for adding run and home page

--- src/routes/running.js
+++ src/routes/running.js
@@ -3,6 +3,12 @@ const router = express.Router();
 
 const showHtmlFile = require('../helpers/route-handlers');
 
-router.get('/', showHtmlFile('run-add.html'));
+router.get('/', showHtmlFile('run-home.html'));
+
+router.get('/add-run', showHtmlFile('run-add.html'));
+
+router.post('/add-run', (req, res, next) => {
+
+});
 
 exports.routes = router;
