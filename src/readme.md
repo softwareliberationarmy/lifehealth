798590b40a66abaf64aec527f755a37feebfb029
Kerry Patrick <kpatrick@mmmicrosystems.com>
2021-08-28 12:03:51 -0500

    sorting runs on the running home page

--- src/readme.md
+++ src/readme.md
@@ -2,11 +2,5 @@ TODO:
 
 General
 - extract shared header info to a single file
-
-Add Run Page
-- default date to today
-
-Home Run Page
-- show all logged runs sorted by run date
-
+- highlight the right section of the app
 
--- src/routes/running.js
+++ src/routes/running.js
@@ -11,10 +11,16 @@ router.get('/add-run', renderTemplate('run-add'));
 
 router.post('/add-run', (req, res) => {
     const newRun = {
-        rundate: req.body.rundate,
+        rundate: req.body.rundate,  //written as a string value
         distance: req.body.distance
     };
     runnings.push(newRun);
+    runnings.sort((r1, r2) => {
+        const secondsDiff = Date.parse(r1.rundate) - Date.parse(r2.rundate);
+        console.log('secondsDiff', secondsDiff);
+        return secondsDiff;
+    } );
+    console.log('runnings', runnings);
     res.redirect('/running');
 });
 
