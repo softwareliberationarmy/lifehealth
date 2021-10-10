9807ca39f4765bb74c45a71940a8c550af4a0b07
Kerry Patrick <kpatrick@mmmicrosystems.com>
2021-10-10 14:34:41 -0500

    deleting sample cypress tests and organizing running area tests a little better

--- src/cypress/integration/running.js
+++ src/cypress/integration/running.js
@@ -1,27 +1,29 @@
 /// <reference types="Cypress" />
 
-describe('running area', () => {
+describe("running area", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000/");
+    cy.contains("li.main-header__item a", "Running").click();
+  });
 
-    beforeEach(() => {
-        cy.visit('http://localhost:3000/');
-        cy.contains('li.main-header__item a', 'Running').click();
-    })
-
-
-    it('can get to the running area', () => {        
-        cy.contains('h1', 'Running!').should('be.visible');
+  describe("on initialization", () => {
+    it("can get to the running area", () => {
+      cy.contains("h1", "Running!").should("be.visible");
     });
 
-    it('should show recent runs', () => {
-        cy.contains('p', 'Recent Runs').should('be.visible');
+    it("should show recent runs", () => {
+      cy.contains("p", "Recent Runs").should("be.visible");
     });
+  });
 
-    it('should be able to add a new run', () => {
-        cy.contains('a', 'Add a run').click();
-        cy.get('[name=rundate]').type('2019-12-31');
-        cy.get('[name=distance]').type('3.2');
-        cy.get('button[type=submit]').click();
+  describe("adding a run", () => {
+    it("should be able to add a new run", () => {
+      cy.contains("a", "Add a run").click();
+      cy.get("[name=rundate]").type("2019-12-31");
+      cy.get("[name=distance]").type("3.2");
+      cy.get("button[type=submit]").click();
 
-        cy.contains('li', '2019-12-31, 3.2 miles').should('be.visible');
+      cy.contains("li", "2019-12-31, 3.2 miles").should("be.visible");
     });
-})
+  });
+});
