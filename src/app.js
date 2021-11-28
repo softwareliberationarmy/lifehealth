346ad7c3067b1230ac6c86d614d68cf380fe6f27
Kerry Patrick <kpatrick@mmmicrosystems.com>
2021-11-28 12:54:13 -0600

    connecting to sql server using sequelize and using env variables for connection info

--- src/app.js
+++ src/app.js
@@ -1,29 +1,40 @@
-const path = require('path');
-const express = require('express');
+require('dotenv').config();
+const path = require("path");
+const express = require("express");
 
-const { renderTemplate } = require('./helpers/route-handlers');
-const running = require('./routes/running');
-const weight = require('./routes/weight');
+const { renderTemplate } = require("./helpers/route-handlers");
+const running = require("./routes/running");
+const weight = require("./routes/weight");
+const sequelize = require("./infra/database");
+const { seq } = require("async");
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', 'src/views');
+app.set("view engine", "ejs");
+app.set("views", "src/views");
 
 //serve static files from public folder
-app.use(express.static(path.join(__dirname, '..', "public")));
-//for parsing HTTP post message body 
-app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, "..", "public")));
+//for parsing HTTP post message body
+app.use(express.urlencoded({ extended: true }));
 
 //set routes
-app.get('/', renderTemplate('index'));
-app.use('/running', running.routes);
-app.use('/weight', weight.routes)
+app.get("/", renderTemplate("index"));
+app.use("/running", running.routes);
+app.use("/weight", weight.routes);
 //construction handler
-for(const area of [{ name: 'weight', route: '/weight'}, { name: 'workouts', route: '/workouts'}]){
-    app.get(area.route, renderTemplate('construction', { area: area.name}));
+for (const area of [
+  { name: "weight", route: "/weight" },
+  { name: "workouts", route: "/workouts" },
+]) {
+  app.get(area.route, renderTemplate("construction", { area: area.name }));
 }
 //last resort handler
-app.use(renderTemplate('notfound'));
+app.use(renderTemplate("notfound"));
 
-app.listen(3000);
+sequelize.sync().then((result) => {
+  app.listen(3000);
+})
+.catch(err => {
+    console.log(err);
+});
---
+++ src/infra/database.js
@@ -0,0 +1,13 @@
+const Sequelize = require("sequelize");
+
+const sequelize = new Sequelize("lifehealth", process.env.DB_USER, process.env.DB_PASSWORD, {
+  dialect: "mssql",
+  host: process.env.DB_HOST,
+  dialectOptions: {
+    options: {
+      instanceName: process.env.DB_INSTANCENAME
+    }
+  }
+});
+
+module.exports = sequelize;
