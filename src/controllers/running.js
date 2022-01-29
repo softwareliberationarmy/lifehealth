9df7fa0af6fd04377facf1c4f6e7b0eed1c0061f
Kerry Patrick <kpatrick@mmmicrosystems.com>
2022-01-29 12:02:48 -0600

    adding additional unit test for posting a new run

--- src/controllers/running.js
+++ src/controllers/running.js
@@ -2,26 +2,24 @@ const Run = require('../models/run');
 
 const { renderTemplate } = require('../helpers/route-handlers');
 
-exports.getHome = (req,res,next) => {
-    Run.findAll()
-    .then(runs => {
-        res.render('run-home', { runnings: runs});
-    });
-}; 
+exports.getHome = (req, res, next) => {
+  Run.findAll().then((runs) => {
+    res.render('run-home', { runnings: runs });
+  });
+};
 
 exports.getAddRun = renderTemplate('run-add');
 
 exports.postAddRun = (req, res) => {
-    console.log('new run', req.body);
-    const newRun = req.user.createRun({
-        runDate: req.body.rundate,
-        miles: req.body.distance
+  const newRun = req.user
+    .createRun({
+      runDate: req.body.rundate,
+      miles: req.body.distance,
     })
-    .then(result => {
-        res.redirect('/running');
+    .then((result) => {
+      res.redirect('/running');
     })
-    .catch(err => {
-        console.log(err);
+    .catch((err) => {
+      console.log(err);
     });
-
 };
--- src/controllers/running.test.js
+++ src/controllers/running.test.js
@@ -2,7 +2,7 @@ jest.mock('../helpers/route-handlers');
 jest.mock('../models/run');
 
 const Run = require('../models/run');
-const { getHome } = require('./running');
+const { getHome, postAddRun } = require('./running');
 const { renderTemplate } = require('../helpers/route-handlers');
 
 describe('running controller', () => {
@@ -24,8 +24,34 @@ describe('running controller', () => {
     });
   });
 
-  //NOTE: this test is called whn
-  describe('it should call renderTemplate for getAddRun', () => {
-    expect(renderTemplate).toBeCalledWith('run-add');
+  describe('GET run-add', () => {
+    it('should call renderTemplate for getAddRun', () => {
+      expect(renderTemplate).toBeCalledWith('run-add');
+    });
+  });
+
+  describe('POST run-add', () => {
+    it('should create a new run and redirect to the running home page', (done) => {
+      const expectedRunDate = new Date();
+      const expectedDistance = 3.1;
+
+      const req = {
+        user: { createRun: jest.fn().mockResolvedValue(Promise.resolve({})) },
+        body: { rundate: expectedRunDate, distance: expectedDistance },
+      };
+      const res = {
+        redirect: (path) => {
+          expect(path).toBe('/running');
+          done();
+        },
+      };
+
+      postAddRun(req, res);
+
+      expect(req.user.createRun).toBeCalledWith({
+        runDate: expectedRunDate,
+        miles: expectedDistance,
+      });
+    });
   });
 });
