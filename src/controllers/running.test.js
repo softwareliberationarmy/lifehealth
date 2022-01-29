038fd8b456547972282099104abd010fe52131bb
Kerry Patrick <kpatrick@mmmicrosystems.com>
2022-01-29 11:26:39 -0600

    adding unit test for get method

--- src/controllers/running.test.js
+++ src/controllers/running.test.js
@@ -1,27 +1,31 @@
-jest.mock('../models/run');
 jest.mock('../helpers/route-handlers');
+jest.mock('../models/run');
 
 const Run = require('../models/run');
 const { getHome } = require('./running');
+const { renderTemplate } = require('../helpers/route-handlers');
 
 describe('running controller', () => {
-    describe('GET Home', () => {
-        it('should pass the runs to the run-home page', (done) => {                        
-            var expected = {}
+  describe('GET Home', () => {
+    it('should pass the runs to the run-home page', (done) => {
+      var expected = {};
 
-            Run.findAll.mockResolvedValue(
-                Promise.resolve(expected)
-            );
+      Run.findAll.mockResolvedValue(Promise.resolve(expected));
 
-            const res = {
-                render: (path,data) => {
-                    expect(path).toBe('run-home');
-                    expect(data.runnings).toBe(expected);
-                    done();
-                }
-            };
+      const res = {
+        render: (path, data) => {
+          expect(path).toBe('run-home');
+          expect(data.runnings).toBe(expected);
+          done();
+        },
+      };
 
-            getHome(null, res, null);
-        });
+      getHome(null, res, null);
     });
+  });
+
+  //NOTE: this test is called whn
+  describe('it should call renderTemplate for getAddRun', () => {
+    expect(renderTemplate).toBeCalledWith('run-add');
+  });
 });
