38385efe854d4baa38bd6699b0d70b50d54aefa3
Kerry Patrick <kpatrick@mmmicrosystems.com>
2022-01-29 11:44:58 -0600

    covering renderTemplate with tests

--- src/helpers/route-handlers.test.js
+++ src/helpers/route-handlers.test.js
@@ -1,19 +1,35 @@
 const path = require('path');
-const { showHtmlFile } = require('./route-handlers');
+const { showHtmlFile, renderTemplate } = require('./route-handlers');
 
 describe('route handling helper methods', () => {
-    describe('showHtmlFile', () => {
-        it('should return a func that sends a file to the response object', () => {
-            const result = showHtmlFile('breadbox.html');
-            const spyResponse = {                 
-                sendFile:  function(fullPath){
-                    this.lastPathSent = fullPath;
-                }
-            };
+  describe('showHtmlFile', () => {
+    it('should return a func that sends a file to the response object', () => {
+      const result = showHtmlFile('breadbox.html');
+      const spyResponse = {
+        sendFile: function (fullPath) {
+          this.lastPathSent = fullPath;
+        },
+      };
 
-            result(null, spyResponse);
+      result(null, spyResponse);
 
-            expect(spyResponse.lastPathSent).toEndWith(path.join('src', 'views', 'breadbox.html'));
-        });
+      expect(spyResponse.lastPathSent).toEndWith(
+        path.join('src', 'views', 'breadbox.html')
+      );
     });
-})
+  });
+
+  describe('renderTemplate', () => {
+    it('should call render and pass in the template name and input', () => {
+      const templateName = 'dummy 1';
+      const input = { someInput: true, value: 0 };
+      const mockRes = { render: jest.fn() };
+
+      const target = renderTemplate(templateName, input);
+
+      target(null, mockRes, null);
+
+      expect(mockRes.render).toBeCalledWith(templateName, input);
+    });
+  });
+});
