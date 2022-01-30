f5eb0c8984930127da0a41c1fdcee01c756d385e
Kerry Patrick <kpatrick@mmmicrosystems.com>
2022-01-30 15:39:57 -0600

    prettified file

--- src/app.js
+++ src/app.js
@@ -1,54 +1,57 @@
-require("dotenv").config();
-const path = require("path");
-const express = require("express");
+require('dotenv').config();
+const path = require('path');
+const express = require('express');
 
-const { renderTemplate } = require("./helpers/route-handlers");
-const running = require("./routes/running");
-const weight = require("./routes/weight");
-const sequelize = require("./infra/database");
+const { renderTemplate } = require('./helpers/route-handlers');
+const running = require('./routes/running');
+const weight = require('./routes/weight');
+const sequelize = require('./infra/database');
 const { dbStartupAndThen } = require('./infra/databaseStartup');
 
-const User = require("./models/user");
-const { defineModelRelationships } = require("./models/relationships");
+const User = require('./models/user');
+const { defineModelRelationships } = require('./models/relationships');
 
 const app = express();
 
-app.set("view engine", "ejs");
-app.set("views", "src/views");
+app.set('view engine', 'ejs');
+app.set('views', 'src/views');
 
 //serve static files from public folder
-app.use(express.static(path.join(__dirname, "..", "public")));
+app.use(express.static(path.join(__dirname, '..', 'public')));
 //for parsing HTTP post message body
 app.use(express.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
   User.findByPk(1)
-  .then(user => {
-    req.user = user;
-    next();
-  })
-  .catch(err => {
-    console.log("Error setting request.user: ", err);
-  })
-})
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => {
+      console.log('Error setting request.user: ', err);
+    });
+});
 
 //set routes
-app.get("/", renderTemplate("index"));
-app.use("/running", running.routes);
-app.use("/weight", weight.routes);
+app.get('/', renderTemplate('index'));
+app.use('/running', running.routes);
+app.use('/weight', weight.routes);
 //construction handler
 for (const area of [
-  { name: "weight", route: "/weight" },
-  { name: "workouts", route: "/workouts" },
+  { name: 'weight', route: '/weight' },
+  { name: 'workouts', route: '/workouts' },
 ]) {
-  app.get(area.route, renderTemplate("construction", { area: area.name }));
+  app.get(area.route, renderTemplate('construction', { area: area.name }));
 }
 //last resort handler
-app.use(renderTemplate("notfound"));
+app.use(renderTemplate('notfound'));
 
 defineModelRelationships();
 
-dbStartupAndThen(() => {app.listen(3000)}
-  , false //don't force changes
-  // , true //force changes
+dbStartupAndThen(
+  () => {
+    app.listen(3000);
+  }
+  // false //(default) don't force changes
+  // true //force changes
 );
