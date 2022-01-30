e3164e49baeeb49161339281d90d419783a49330
Kerry Patrick <kpatrick@mmmicrosystems.com>
2022-01-30 15:40:07 -0600

    replacing mysql installation with sqlite

--- src/infra/database.js
+++ src/infra/database.js
@@ -1,14 +1,8 @@
-const Sequelize = require("sequelize");
+const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize("lifehealth", process.env.DB_USER, process.env.DB_PASSWORD, {
-  dialect: "mssql",
-  host: process.env.DB_HOST,
-  dialectOptions: {
-    options: {
-      instanceName: process.env.DB_INSTANCENAME,
-      useUTC: false
-    }
-  }
+const sequelize = new Sequelize({
+  dialect: 'sqlite',
+  storage: 'data/lifehealth.sqlite',
 });
 
 module.exports = sequelize;
